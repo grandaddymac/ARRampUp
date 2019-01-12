d87b5fdc339f35a79111fa8e5e23774101d7ead1
gdm <gdm.mbp@gdmMBP.local>
2019-01-11 19:27:45 -0500

    Setup RampPicker with ramps and RampPlacer to add to sceneView

---
+++ ARRampUp/Controller/RampPickerVC.swift
@@ -0,0 +1,70 @@
+//
+//  RampPickerVC.swift
+//  ARRampUp
+//
+//  Created by gdm on 1/10/19.
+//  Copyright © 2019 gdm. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class RampPickerVC: UIViewController {
+
+    var sceneView: SCNView!
+    var size: CGSize!
+    weak var rampPlacerVC: RampPlacerVC!
+    
+    init(size: CGSize) {
+        super.init(nibName: nil, bundle: nil)
+        self.size = size
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.frame = CGRect(origin: CGPoint.zero, size: size)
+        sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        view.insertSubview(sceneView, at: 0)
+        preferredContentSize = size
+        
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        sceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tap)
+        
+        let pipe = Ramp.getPipe()
+        Ramp.startRotation(node: pipe)
+        scene.rootNode.addChildNode(pipe)
+        
+        let pyramid = Ramp.getPyramid()
+        Ramp.startRotation(node: pyramid)
+        scene.rootNode.addChildNode(pyramid)
+        
+        let quarter = Ramp.getQuarter()
+        Ramp.startRotation(node: quarter)
+        scene.rootNode.addChildNode(quarter)
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UIGestureRecognizer) {
+        let p = gestureRecognizer.location(in: sceneView)
+        let hitresults = sceneView.hitTest(p, options: [:])
+        
+        if hitresults.count > 0 {
+            let node = hitresults[0].node
+            print(node.name!)
+            rampPlacerVC.onRampSelected(node.name!)
+        }
+    }
+   
+
+}
